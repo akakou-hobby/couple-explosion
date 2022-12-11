659107fe0281433ccfd2b08b8d94405e52b7d72c
akakou <akakou571@gmail.com>
2022-12-11 15:12:01 +0900

    リファクタリング

---
+++ detect.js
@@ -0,0 +1,40 @@
+function startDetecting() {
+    alert('hello')
+
+    const status = select('#status');
+    status.html('モデルを読み込んだ');
+
+    (async () => {
+        while (true) {
+            await detect();
+            render();
+        }
+    })()
+
+}
+
+function detect() {
+    return new Promise(resolve => {
+        yolo.detect(function (err, results) {
+            objects = results;
+            console.log(objects)
+            resolve()
+        });
+    })
+}
+
+function render() {
+    image(video, 0, 0, width, height);
+
+    for (let i = 0; i < objects.length; i++) {
+        noStroke();
+        fill(0, 255, 0);
+
+        text(objects[i].className, objects[i].x * width, objects[i].y * height - 5);
+        noFill();
+        strokeWeight(4);
+        stroke(0, 255, 0);
+
+        rect(objects[i].x * width, objects[i].y * height, objects[i].w * width, objects[i].h * height);
+    }
+}
